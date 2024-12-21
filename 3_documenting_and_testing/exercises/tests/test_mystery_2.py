5a01c9a4754d5f290d7ecd9c9df151c152525f48
Ahmed Hussein <ahmed.khalifa11220099@gmail.com>
2024-12-21 14:33:21 +0000

    Solved test for mystery2

--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -1,14 +1,40 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 20/12/2024
+
+@author: Ahmed Khalifa
+"""
 import unittest
 
-from ..mystery_2 import mystery_2
+from ..length import length
 
-class TestMystery2(unittest.TestCase):
-    """ """
+class TestLength(unittest.TestCase):
+    """ Test the length function """
+ 
+    def test_empty_list(self):
+        """It should evaluate [] to 0"""
+        actual = length([])
+        expected = 0
+        self.assertEqual(actual, expected)
 
-    def test_minimal_list_input(self):
-        """"""
-        self.assertEqual(mystery_2([]), None)
+    def test_list_with_one_element(self):
+        """It should evaluate [3] to 1"""
+        actual = length([3])
+        expected = 1
+        self.assertEqual(actual, expected)
 
-    def test_minimal_string_input(self):
-        """"""
-        self.assertEqual(mystery_2(''), None)
+    def test_list_with_duplicate_elements(self):
+        """It should evaluate [3,3] to 2"""
+        actual = length([3, 3])
+        expected = 2
+        self.assertEqual(actual, expected)
+        
+    def test_list_with_10_elements(self):
+        """It should evaluate [1,2,4,5,6,7,8,0,0,4] to 10"""
+        actual = length([1,2,4,5,6,7,8,0,0,4])
+        expected = 10
+        self.assertEqual(actual, expected)
+        
+    #def test_if_parameter_is_not_a_list(self):
+        #assert isinstance(length(a), list)
